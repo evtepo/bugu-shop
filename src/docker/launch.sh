6722592749538779a083a53905fa53d3d2b13d30
evtepo <andrey_laguta00@mail.ru>
2024-07-31 16:49:53 +1000

    исправить команды

--- src/docker/launch.sh
+++ src/docker/launch.sh
@@ -4,7 +4,7 @@ while ! nc -z $DJANGO_DATABASE_HOST $DJANGO_DATABASE_PORT; do
       sleep 0.1
 done 
 
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 gunicorn --bind 0.0.0.0:8000 src.wsgi:application
